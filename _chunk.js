d203b90e533bbf1ce30480807882c27827578c33
chuandong.huang <chuandong.huang@dmall>
2022-04-28 20:15:56 +0800

    feat:_chunk

--- _chunk.js
+++ _chunk.js
@@ -1,13 +1,11 @@
 function _chunk(arr,size){
     const newArr = []
     for (let i = 0; i < arr.length; i++) {
-        
         newArr[i] = arr.splice(0,size)
         if(arr.length <= size){
             newArr.push(arr)
             break
         }
     }
+    return newArr
 }
-    
-_chunk([1,2,3,4,5,6,7,8,9,10],6)
